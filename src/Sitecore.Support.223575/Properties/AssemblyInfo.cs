7b8b4f7f68238cd15776f8949283ea585ffeff10
DK\dmsa <dmsa@sitecore.net>
2018-06-05 15:54:15 +0300

    #223575: Origin code

---
+++ src/Sitecore.Support.223575/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.223575")]
+[assembly: AssemblyProduct("Sitecore.Support.223575")]
+[assembly: ComVisible(false)]
